8c932b1ce4b6d96b4438b599c6ec6d34cce49fc4
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-05-05 10:53:59 +0800

    Update: use bootstrap for reusability

--- backend/account/register.php
+++ backend/account/register.php
@@ -1,22 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://127.0.0.1:3000");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    echo json_encode(['message' => 'Preflight check successful']);
-    exit();
-}
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    echo json_encode(['error' => 'Method not allowed']);
-    exit();
-}
-
+require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . '/../utilities/connect_db.php';
 require_once __DIR__ . '/../email/send_verification.php'; // imports sendVerificationEmail()
 
